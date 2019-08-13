080cc09806e79d4507ef627b0606718a2ec2e9f5
kbindustries <kevbarclay@gmail.com>
2019-08-13 19:17:29 +0100

    Added new view model property HideCompletedTasks to store current list state toggle.

--- Todo/Models/TodoLists/TodoListDetailViewmodel.cs
+++ Todo/Models/TodoLists/TodoListDetailViewmodel.cs
@@ -8,12 +8,14 @@ namespace Todo.Models.TodoLists
         public int TodoListId { get; }
         public string Title { get; }
         public ICollection<TodoItemSummaryViewmodel> Items { get; }
+        public bool HideCompletedTasks { get; }
 
-        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items)
+        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items, bool hideCompletedTasks)
         {
             Items = items;
             TodoListId = todoListId;
             Title = title;
+            HideCompletedTasks = hideCompletedTasks;
         }
     }
 }
