4571691fb317a097d862a896cbff5ad63f091740
kbindustries <kevbarclay@gmail.com>
2019-08-13 22:26:22 +0100

    Filter code moved from controller to context helper class.

--- Todo/Services/ApplicationDbContextConvenience.cs
+++ Todo/Services/ApplicationDbContextConvenience.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.Data;
 using Todo.Data.Entities;
+using Todo.Models.TodoLists;
 
 namespace Todo.Services
 {
@@ -12,6 +13,38 @@ namespace Todo.Services
             return dbContext.TodoLists.Include(tl => tl.Owner)
                 .Include(tl => tl.Items)
                 .Where(tl => tl.Owner.Id == userId || tl.Items.Any(i => i.ResponsibleParty.Id == userId));
+        }
+
+        // TODO: Refactor - filters should operate on an IQueryable rather than materialised lists (let the db sort the data)
+        public static TodoList FilteredTodoList(this ApplicationDbContext dbContext, int todoListId, TodoListFilters filters)
+        {
+            var todoList = dbContext.TodoLists
+                .Include(tl => tl.Owner)
+                .Include(tl => tl.Items)
+                .ThenInclude(ti => ti.ResponsibleParty)
+                .Single(tl => tl.TodoListId == todoListId);
+
+            if (filters.HideCompletedTasks)
+            {
+                todoList.Items = todoList.Items
+                    .Where(i => !i.IsDone)
+                    .ToList();
+            }
+
+            if (filters.OrderByDescendingRank)
+            {
+                todoList.Items = todoList.Items
+                    .OrderByDescending(i => i.Rank)//.ThenBy(i => i.Importance)
+                    .ToList();
+            }
+            else
+            {
+                todoList.Items = todoList.Items
+                    .OrderBy(i => i.Rank)//.ThenBy(i => i.Importance)
+                    .ToList();
+            }
+
+            return todoList;
         }
 
         public static TodoList SingleTodoList(this ApplicationDbContext dbContext, int todoListId)
